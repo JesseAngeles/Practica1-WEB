2dd0b1f39cdc6f5b68ab16784faebc492d50e1ca
Jesse <73973352+JesseAngeles@users.noreply.github.com>
2024-04-06 22:54:48 -0600

    index, ejercicios, informacion personal

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -1,8 +1,10 @@
 // EJERCICIO 1: Leer un numero y mostrar si dicho valor es par o impar
 document.getElementById(`ej1`).addEventListener(`click`, function () {
-    let num = document.getElementById(`in_ej1`).value || 0;
+    const num = document.getElementById(`in_ej1`).value || 0;
+    const res = document.getElementById(`res_ej1`);
+
     if (Math.trunc(num) == num) {
-        (num % 2 == 0) ? alert(num + ` es par`) : alert(num + ` es impar`);
+        res.innerHTML = (num % 2 == 0) ? (num + ` es par`) : (num + ` es impar`);
     } else {
         alert(`Ingrese un numero valido`);
     }
@@ -10,8 +12,8 @@ document.getElementById(`ej1`).addEventListener(`click`, function () {
 
 // EJERCICIO 2: Leer un numero y mostrar su tabla de multiplicar
 document.getElementById(`ej2`).addEventListener(`click`, function () {
-    let num = document.getElementById(`num_ej2`).value || 0;
-    let max = document.getElementById(`max_ej2`).value || 0;
+    const num = document.getElementById(`num_ej2`).value || 0;
+    const max = document.getElementById(`max_ej2`).value || 0;
     if (Math.trunc(max) == max) {
         let table = new Array(max);
         let res = document.getElementById(`res2`);
@@ -34,9 +36,9 @@ document.getElementById(`ej2`).addEventListener(`click`, function () {
 
 // EJERCICIO 3: Leer dos numeros y mostrar su producto mediante sumas
 document.getElementById(`ej3`).addEventListener(`click`, function () {
-    let mul1 = document.getElementById(`mul1_ej3`).value || 0;
-    let mul2 = document.getElementById(`mul2_ej3`).value || 0;
-    let res = document.getElementById(`res3`);
+    const mul1 = document.getElementById(`mul1_ej3`).value || 0;
+    const mul2 = document.getElementById(`mul2_ej3`).value || 0;
+    const res = document.getElementById(`res3`);
     let multi = 0;
 
     if (Math.trunc(mul2) == mul2 && mul2 >= 0) {
@@ -62,11 +64,11 @@ document.getElementById(`ej3`).addEventListener(`click`, function () {
 
 // EJERCICIO 4: Leer una secuencia de n numeros almacenadosen un arreglo y mostrar la posicion donde se encuentra el mayor valor leido
 let values_ej4 = [];
-let pos_ej4 = document.getElementById(`pos_ej4`);
-let val_ej4 = document.getElementById(`val_ej4`);
+const pos_ej4 = document.getElementById(`pos_ej4`);
+const val_ej4 = document.getElementById(`val_ej4`);
 
 document.getElementById(`add_ej4`).addEventListener(`click`, function () {
-    let num = Number(document.getElementById(`in_ej4`).value) || 0;
+    const num = Number(document.getElementById(`in_ej4`).value) || 0;
     values_ej4.push(num);
     updateTableEj4();
 });
@@ -79,7 +81,7 @@ document.getElementById('clear_ej4').addEventListener(`click`, function () {
 document.getElementById(`find_ej4`).addEventListener(`click`, function () {
     if (values_ej4.length > 0) {
         let numericValues = values_ej4.map(Number);
-        let maxValue = Math.max(...numericValues);
+        const maxValue = Math.max(...numericValues);
         alert(`Valor máximo: ${maxValue} en la posición: ${numericValues.indexOf(maxValue)}`);
     } else {
         alert(`Sin valor máximo`);
@@ -100,13 +102,13 @@ function updateTableEj4() {
 let a_values = [];
 let b_values = [];
 let c_values = [];
-let a = document.getElementById(`a_ej5`);
-let b = document.getElementById(`b_ej5`);
-let c = document.getElementById(`c_ej5`);
+const a = document.getElementById(`a_ej5`);
+const b = document.getElementById(`b_ej5`);
+const c = document.getElementById(`c_ej5`);
 
 document.getElementById(`add_ej5`).addEventListener(`click`, function () {
-    let a_val = Number(document.getElementById(`in1_ej5`).value) || 0;
-    let b_val = Number(document.getElementById(`in2_ej5`).value) || 0;
+    const a_val = Number(document.getElementById(`in1_ej5`).value) || 0;
+    const b_val = Number(document.getElementById(`in2_ej5`).value) || 0;
 
     a_values.push(a_val);
     b_values.push(b_val);
@@ -136,11 +138,11 @@ function updateTableEj5() {
 
 // EJERCICIO 6: Calcular la media de una secuencia de numeros proporcionados por el usuario
 let values_ej6 = [];
-let pos_ej6 = document.getElementById(`pos_ej6`);
-let val_ej6 = document.getElementById(`val_ej6`);
+const pos_ej6 = document.getElementById(`pos_ej6`);
+const val_ej6 = document.getElementById(`val_ej6`);
 
 document.getElementById(`add_ej6`).addEventListener(`click`, function () {
-    let num = Number(document.getElementById(`in_ej6`).value) || 0;
+    const num = Number(document.getElementById(`in_ej6`).value) || 0;
     values_ej6.push(num);
     updateTableEj6();
 });
@@ -156,7 +158,7 @@ document.getElementById(`average_ej6`).addEventListener(`click`, function () {
         for (let i = 0; i < values_ej6.length; i++) {
             sum += values_ej6[i];
         }
-        let average = sum / values_ej6.length;
+        const average = sum / values_ej6.length;
         alert(`Promedio ${average}`);
     } else {
         alert(`Sin valores`);
@@ -174,6 +176,67 @@ function updateTableEj6() {
 }
 
 // EJERCICIO 7: Dada una secuencia de numeros leidos y almacenados en un arreglo A mostrar dichos numeros en orden
+let values_ej7 = [];
+const pos_ej7 = document.getElementById(`pos_ej7`);
+const val_ej7 = document.getElementById(`val_ej7`);
 
+document.getElementById(`add_ej7`).addEventListener(`click`, function () {
+    const num = Number(document.getElementById(`in_ej7`).value) || 0;
+    values_ej7.push(num);
+    updateTableEj7();
+});
+
+document.getElementById(`clear_ej7`).addEventListener(`click`, function () {
+    values_ej7 = [];
+    updateTableEj7();
+});
+
+document.getElementById(`order_ej7`).addEventListener(`click`, function () {
+    quickSort(values_ej7, 0, values_ej7.length - 1);
+    updateTableEj7();
+});
+
+function quickSort(arr, start, end) {
+    if (start < end) {
+        let pivotIndex = partition(arr, start, end);
+        quickSort(arr, start, pivotIndex - 1); 
+        quickSort(arr, pivotIndex + 1, end);   
+    }
+    return arr;
+}
+
+function partition(arr, start, end) {
+    const pivotValue = arr[end];
+    let pivotIndex = start; 
+    for (let i = start; i < end; i++) {
+        if (arr[i] < pivotValue) {
+            [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]];
+            pivotIndex++;
+        }
+    }
+    
+    [arr[pivotIndex], arr[end]] = [arr[end], arr[pivotIndex]];
+    return pivotIndex;
+}
+
+function updateTableEj7() {
+    pos_ej7.innerHTML = `<td>Posiciones</td>`;
+    val_ej7.innerHTML = `<td>Valores</td>`;
+
+    for (let i = 0; i < values_ej7.length; i++) {
+        pos_ej7.innerHTML += `<td>${i}</td>`;
+        val_ej7.innerHTML += `<td>${values_ej7[i]}</td>`;
+    }
+}
 
 // EJERCICIO 8: Dado una secuencia de textos proporcionados por el usuario visualizar: la longitud, en mayusculas, minusculas, cada uno de ellas
+const lower = document.getElementById(`lower_ej8`);
+const upper = document.getElementById(`upper_ej8`);
+const lengthText = document.getElementById(`length_ej8`);
+
+document.getElementById(`analize_ej8`).addEventListener(`click`, function () {
+    const text = document.getElementById(`in_ej8`).value || '';
+    lengthText.innerHTML = `${text.length}`;
+    lower.innerHTML = `${text.toLowerCase()}`;
+    upper.innerHTML = `${text.toUpperCase()}`;
+});
